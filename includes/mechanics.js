9d93da33ed7f83baf9a8e4e8f944f08520f643dc
jakemadness <j4kem4dness@live.com>
2019-02-14 21:29:56 +0000

    Create mechanics.js

---
+++ includes/mechanics.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+   var self        = this
+   self.helloWorld = function(message) {
+      message.channel.send('hello world!');
+   };
+   self.say = function(message, text) {
+      message.channel.send(text);
+   };
+   self.avatar = function(message) {
+      console.log({"embed": {
+         'title': message.author.username + '\'s avatar',
+         'color': 2338864,
+         'image': {
+            'url': message.author.avatarURL
+         }
+      }});
+   };
+   self.parse = function(message) {
+      for ( var i = 0, length = config.commands.length, match = false; i < length; i++ ) {
+         if ( config.commands[i].disabled ) {
+            continue;
+         }
+         else {
+            match = message.content.match(config.commands[i].pattern);
+            if ( match ) {
+               match.shift();
+               match.unshift(message);
+               self[config.commands[i].callback].apply(null, match);
+            }
+         }
+      }
+   };
+};
