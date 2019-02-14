7a8e34998514356c1faf5792b26dc31cd9ad517f
jakemadness <j4kem4dness@live.com>
2019-02-14 21:26:55 +0000

    Update config.js

--- includes/config.js
+++ includes/config.js
@@ -3,18 +3,18 @@ module.exports = function() {
    self.token    = 'discord bot token';
    self.commands = [
       {
-         'pattern':  /^b\!hello$/i,
+         'pattern':  /^b!hello$/i,
          'callback': 'helloWorld',
          'disabled': false
       },
       {
-         'pattern':  /^b\!avatar\s*\<\@([0-9]{1,50})\>|b\!avatar$/i,
-         'callback': 'avatar',
+         'pattern':  /^b!say\s*(.+)$/i,
+         'callback': 'say',
          'disabled': false
       },
       {
-         'pattern':  /^b\!say\s*(.+)$/i,
-         'callback': 'say',
+         'pattern':  /^b!avatar\s*\<\@([0-9]{1,50})\>|b!avatar$/i,
+         'callback': 'avatar',
          'disabled': false
       }
    ];
