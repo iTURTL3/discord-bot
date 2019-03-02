70f0eaeff337ced5ee518a5431aec697f43d8658
jakemadness <j4kem4dness@live.com>
2019-03-02 07:27:46 +0000

    Update config.js

--- includes/config.js
+++ includes/config.js
@@ -1,22 +1,47 @@
 module.exports = function() {
-   var self      = this;
-   self.token    = 'NVQ0MjH0MsEyODg1MzO5OTIq.D5HtsA.Aj2G5XOch3EgkCb6pvNLcrdyKwY';
-   self.prefix   = 'b!';
-   self.commands = [
+   var self          = this;
+   self.token        = '';
+   self.name         = 'Example Bot';
+   self.abbreviation = 'eb';
+   self.prefix       = self.abbreviation + '!';
+   self.inviteUrl    = 'https://discordapp.com/oauth2/authorize?client_id=&scope=bot&permissions=';
+   self.embedColor   = 0x00FF00;
+   self.startTime    = Date.now();
+   self.commands     = [
       {
-         'pattern':  new RegExp('^' + self.prefix + 'ping', 'i'),
-         'callback': 'ping',
-         'disabled': false
+         'pattern':     new RegExp('^' + self.prefix + 'say\\s*(.+)$', 'i'),
+         'description': 'make me say something',
+         'example':     self.prefix + 'say hello world!',
+         'callback':    'say',
+         'disabled':    false
       },
       {
-         'pattern':  new RegExp('^' + self.prefix + 'say\\s*(.+)$', 'i'),
-         'callback': 'say',
-         'disabled': false
+         'pattern':     new RegExp('^' + self.prefix + 'invite$', 'i'),
+         'description': 'invite me to your server',
+         'example':     self.prefix + 'invite',
+         'callback':    'invite',
+         'disabled':    false
       },
       {
-         'pattern':  new RegExp('^' + self.prefix + 'avatar$', 'i'),
-         'callback': 'avatar',
-         'disabled': false
+         'pattern':     new RegExp('^' + self.prefix + 'ping$', 'i'),
+         'description': 'check if i am alive',
+         'example':     self.prefix + 'ping',
+         'callback':    'pingPong',
+         'disabled':    false
+      },
+      {
+         'pattern':     new RegExp('^' + self.prefix + 'up\\s*time$', 'i'),
+         'description': 'check how long i have been alive for',
+         'example':     self.prefix + 'uptime',
+         'callback':    'upTime',
+         'disabled':    false
+      },
+      {
+         'pattern':     new RegExp('^' + self.prefix + 'help$', 'i'),
+         'description': 'display my commands',
+         'example':     self.prefix + 'help',
+         'callback':    'commandList',
+         'disabled':    false
       }
    ];
 };
