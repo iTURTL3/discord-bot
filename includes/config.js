0f8663c3703369aae4dbc31597816e1da275d9c7
jakemadness <j4kem4dness@live.com>
2019-03-03 15:08:31 +0000

    Update config.js

--- includes/config.js
+++ includes/config.js
@@ -6,7 +6,6 @@ module.exports = function() {
    self.prefix       = self.abbreviation + '!';
    self.inviteUrl    = 'https://discordapp.com/oauth2/authorize?client_id=&scope=bot&permissions=';
    self.embedColor   = 0x00FF00;
-   self.startTime    = Date.now();
    self.permissions  = [
       'READ_MESSAGES',
       'SEND_MESSAGES',
@@ -19,35 +18,48 @@ module.exports = function() {
          'description': 'make me say something',
          'example':     self.prefix + 'say <message>',
          'callback':    'say',
-         'disabled':    false
+         'disabled':    false,
+         'private':     false
       },
       {
          'pattern':     new RegExp('^' + self.prefix + 'invite$', 'i'),
          'description': 'invite me to your server',
          'example':     self.prefix + 'invite',
          'callback':    'invite',
-         'disabled':    false
+         'disabled':    false,
+         'private':     false
       },
       {
          'pattern':     new RegExp('^' + self.prefix + 'ping$', 'i'),
          'description': 'check if i am alive',
          'example':     self.prefix + 'ping',
          'callback':    'pingPong',
-         'disabled':    false
+         'disabled':    false,
+         'private':     false
       },
       {
          'pattern':     new RegExp('^' + self.prefix + 'up\\s*time$', 'i'),
          'description': 'check how long i have been alive for',
          'example':     self.prefix + 'uptime',
          'callback':    'upTime',
-         'disabled':    false
+         'disabled':    false,
+         'private':     false
+      },
+      {
+         'pattern':     new RegExp('^' + self.prefix + 'servers$', 'i'),
+         'description': 'check how many servers i am in',
+         'example':     self.prefix + 'servers',
+         'callback':    'serverCount',
+         'disabled':    false,
+         'private':     false
       },
       {
          'pattern':     new RegExp('^' + self.prefix + 'help$', 'i'),
          'description': 'display my commands',
          'example':     self.prefix + 'help',
          'callback':    'commandList',
-         'disabled':    false
+         'disabled':    false,
+         'private':     true
       }
    ];
 };
