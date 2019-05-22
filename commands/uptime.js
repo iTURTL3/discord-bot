f7da6009dcc201ef871213595a76a6f8b9a64661
jakemadness <j4kem4dness@live.com>
2019-05-22 16:52:57 +0100

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,19 @@
+module.exports = function(bot, config, data, utilities, callbacks) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'uptime$', 'i'),
+      'description': 'check how long i have been alive for',
+      'example':     config.prefix + 'uptime',
+      'callback':    'upTime',
+      'cooldown':    2000,
+      'disabled':    false,
+      'private':     false
+   });
+   callbacks.upTime = function(message) {
+      var time = utilities.msToDuration(bot.uptime);
+      message.channel.send({'embed': {
+         'color':       config.embedColor,
+         'title':       config.name + ' - Up Time',
+         'description': 'i have been alive for ' + time['0'] + 'd ' + time['1'] + 'h ' + time['2'] + 'm ' + time['3'] + 's!'
+      }});
+   };
+};
