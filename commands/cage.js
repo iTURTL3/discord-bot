d2a85102f0c2662808586f72eb2067230e99b720
jakemadness <j4kem4dness@live.com>
2019-05-22 18:23:01 +0100

    Create cage.js

---
+++ commands/cage.js
@@ -0,0 +1,26 @@
+module.exports = function(bot, config, data, utilities, callbacks) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'cage$', 'i'),
+      'description': 'make me post a random nicolas cage image',
+      'example':     config.prefix + 'cage',
+      'callback':    'nicolasCage',
+      'cooldown':    2000,
+      'disabled':    false,
+      'private':     false
+   });
+   data.nicolasCageImageIds = [
+      'zWuL7JS',
+      'M75C4s9',
+      'hGKti2R',
+      'W3YaSyr',
+      'marT0q3',
+      'NsiWHlc',
+      'XLkZvFG'
+   ];
+   callbacks.nicolasCage = function(message) {
+      message.channel.send(text, {'files': [{
+         'name':       config.name + '.jpg',
+         'attachment': 'https://i.imgur.com/' + utilities.randomArrayValue(data.nicolasCageImageIds) + '.jpg'
+      }]});
+   };
+};
