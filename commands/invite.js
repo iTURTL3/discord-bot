25b7b03dddf67863a247cb513a22a7faa47c92d1
jakemadness <j4kem4dness@live.com>
2019-05-22 16:49:39 +0100

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,18 @@
+module.exports = function(bot, config, data, utilities, callbacks) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'invite$', 'i'),
+      'description': 'invite me to your server',
+      'example':     config.prefix + 'invite',
+      'callback':    'inviteLink',
+      'cooldown':    2000,
+      'disabled':    false,
+      'private':     false
+   });
+   callbacks.inviteLink = function(message) {
+      message.channel.send({'embed': {
+         'color':       config.embedColor,
+         'title':       config.name + ' - Invite',
+         'description': '[invite me to your server!](' + config.invite + ')'
+      }});
+   };
+};
