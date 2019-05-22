fe9f3cd9839d2e386bdea4b3293f4f8623df86ee
jakemadness <j4kem4dness@live.com>
2019-05-22 16:44:14 +0100

    Update events.js

--- includes/events.js
+++ includes/events.js
@@ -1,19 +1,30 @@
-module.exports = function(bot, config, callbacks) {
+module.exports = function(bot, config, data, utilities, callbacks) {
    var self   = this;
    self.error = function(error) {
       console.log(error);
    };
+   self.ready = function() {
+      bot.user.setStatus('dnd');
+      bot.user.setActivity('You, ' + config.prefix + 'help', {type: 'WATCHING'});
+   };
    self.message = function(message) {
-      if ( message.guild.me.permissions.has(config.permissions) && message.content.indexOf(config.prefix) === 0 && !message.author.bot ) {
-         for ( var i = 0, length = config.commands.length, match = false; i < length; i++ ) {
-            if ( !config.commands[i].disabled ) {
-               match = message.content.match(config.commands[i].pattern);
-               if ( match ) {
+      if ( message.content.indexOf(config.prefix) === 0 && !message.author.bot ) {
+         for ( var i = data.commands.length - 1, match = false; i >= 0; i-- ) {
+            match = message.content.match(data.commands[i].pattern);
+            if ( match ) {
+               if ( data.commands[i].disabled ) {
+                  message.channel.send('this command is currently disabled!');
+               }
+               else if ( message.author.id in data.cooldowns ) {
+                  message.channel.send('you can use another command in **' + utilities.shortenFloat((data.cooldowns[message.author.id] - message.createdTimestamp) / 1000, 2) + '**s!');
+               }
+               else {
                   match.shift();
                   match.unshift(message);
-                  callbacks[config.commands[i].callback].apply(null, match);
-                  break;
+                  callbacks[data.commands[i].callback].apply(null, match);
+                  utilities.objectSetAndDelete(data.cooldowns, message.author.id, message.createdTimestamp + data.commands[i].cooldown, data.commands[i].cooldown);
                }
+               break;
             }
          }
       }
