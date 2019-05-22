812c07fe0507bf91559d7561f96ec8e53d9dfae8
jakemadness <j4kem4dness@live.com>
2019-05-22 17:08:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
+// modules.
 var bot       = new (require('discord.js')).Client();
 var config    = new (require('./includes/config.js'))();
 var data      = new (require('./includes/data.js'))();
 var utilities = new (require('./includes/utilities.js'))();
 var callbacks = new (require('./includes/callbacks.js'))();
 var events    = new (require('./includes/events.js'))(bot, config, data, utilities, callbacks);
+
+// register commands.
 require('./commands/avatar.js')(bot, config, data, utilities, callbacks);
 require('./commands/invite.js')(bot, config, data, utilities, callbacks);
 require('./commands/ping.js')(bot, config, data, utilities, callbacks);
@@ -11,6 +14,8 @@ require('./commands/uptime.js')(bot, config, data, utilities, callbacks);
 require('./commands/servers.js')(bot, config, data, utilities, callbacks);
 require('./commands/version.js')(bot, config, data, utilities, callbacks);
 require('./commands/help.js')(bot, config, data, utilities, callbacks);
+
+// set events
 bot.on('error',   events.error);
 bot.on('ready',   events.ready);
 bot.on('message', events.message);
