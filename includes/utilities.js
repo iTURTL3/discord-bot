3cb3dc18eda25cfc5cc276dc5de17b9ad9e916e1
jakemadness <j4kem4dness@live.com>
2019-05-22 16:56:15 +0100

    Update utilities.js

--- includes/utilities.js
+++ includes/utilities.js
@@ -3,6 +3,14 @@ module.exports = function() {
    self.shortenFloat = function(number, places) {
       return Number(number.toFixed(places));
    };
+   self.msToDuration = function(ms) {
+      return [
+         Math.floor(ms  / (1000 * 60   * 60 * 24)),
+         Math.floor((ms / (1000 * 60   * 60)) % 24),
+         Math.floor((ms / (1000 * 60)) % 60),
+         Math.floor((ms / 1000) % 60)
+      ];
+   };
    self.objectSetAndDelete = function(scope, name, value, delay) {
       scope[name] = value;
       setTimeout(function() {
