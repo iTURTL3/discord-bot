e971d1762b5bbf60221e3146466b14decf0edf80
jakemadness <j4kem4dness@live.com>
2019-05-23 19:14:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,10 @@
-// modules.
 var bot       = new (require('discord.js')).Client();
 var config    = new (require('./includes/config.js'))();
 var data      = new (require('./includes/data.js'))();
 var utilities = new (require('./includes/utilities.js'))();
 var callbacks = new (require('./includes/callbacks.js'))();
 var events    = new (require('./includes/events.js'))(bot, config, data, utilities, callbacks);
-
-// register commands.
-require('./commands/avatar.js')(bot, config, data, utilities, callbacks);
-require('./commands/say.js')(bot, config, data, utilities, callbacks);
-require('./commands/cage.js')(bot, config, data, utilities, callbacks);
-require('./commands/invite.js')(bot, config, data, utilities, callbacks);
-require('./commands/ping.js')(bot, config, data, utilities, callbacks);
-require('./commands/uptime.js')(bot, config, data, utilities, callbacks);
-require('./commands/servers.js')(bot, config, data, utilities, callbacks);
-require('./commands/version.js')(bot, config, data, utilities, callbacks);
-require('./commands/help.js')(bot, config, data, utilities, callbacks);
-
-// set events
+require('./commands/register.js')(bot, config, data, utilities, callbacks);
 bot.on('error',   events.error);
 bot.on('ready',   events.ready);
 bot.on('message', events.message);
