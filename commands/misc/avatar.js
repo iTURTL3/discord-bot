b555be23b4bb56dfb778163ab395723480409548
jakemadness <j4kem4dness@live.com>
2019-06-15 02:24:44 +0100

    Create avatar.js

---
+++ commands/misc/avatar.js
@@ -0,0 +1,20 @@
+module.exports = function(bot, config, data, utilities, callbacks) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'avatar\\s*(\\<\\@\\!?[0-9]+\\>)?$', 'i'),
+      'description': 'display somebodys avatar',
+      'example':     config.prefix + 'avatar <mention>',
+      'callback':    'avatar',
+      'category':    'misc',
+      'cooldown':    2000,
+      'disabled':    false,
+      'private':     false
+   });
+   callbacks.avatar = function(message) {
+      var user      = message.mentions.users.first() || message.author;
+      var extension = user.avatarURL.match(/\.(png|jpg|jpeg|gif)/)['0'];
+      message.channel.send({'files': [{
+         'name':       config.name + extension,
+         'attachment': 'https://cdn.discordapp.com/avatars/' + user.id + '/' + user.avatar + extension
+      }]});
+   };
+};
