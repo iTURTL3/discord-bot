7d42939a7e8c837b9e483d3a3fce55460870fcf7
jakemadness <j4kem4dness@live.com>
2019-06-15 02:21:01 +0100

    Create version.js

---
+++ commands/bot/version.js
@@ -0,0 +1,19 @@
+module.exports = function(bot, config, data, utilities, callbacks) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'version$', 'i'),
+      'description': 'check my current version',
+      'example':     config.prefix + 'version',
+      'callback':    'botVersion',
+      'category':    'bot',
+      'cooldown':    2000,
+      'disabled':    false,
+      'private':     false
+   });
+   callbacks.botVersion = function(message) {
+      message.channel.send({'embed': {
+         'color':       config.embedColor,
+         'title':       config.name + ' - Version',
+         'description': 'my current version is ' + config.version + '!'
+      }});
+   };
+};
