daf514bb8a4266f9b0bfbc6e73f4bcde95aa8280
jakemadness <j4kem4dness@live.com>
2019-08-27 12:37:10 +0100

    Update cage.js

--- commands/misc/cage.js
+++ commands/misc/cage.js
@@ -7,7 +7,8 @@ module.exports = function(bot, config, data, utilities, callbacks) {
       'category':    'misc',
       'cooldown':    2000,
       'disabled':    false,
-      'private':     false
+      'private':     false,
+      'nsfw':        false
    });
    data.nicolasCageImageIds = [
       'zWuL7JS',
