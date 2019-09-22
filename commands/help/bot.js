13ff0f7fa96cb82e6651d4d3727975bb212a36c7
jakemadness <j4kem4dness@live.com>
2019-09-22 16:14:16 +0100

    Update bot.js

--- commands/help/bot.js
+++ commands/help/bot.js
@@ -1,24 +1,21 @@
-module.exports = function(bot, config, data, utilities, callbacks) {
+module.exports = function(bot, config, data, utilities, dependencies) {
    data.commands.push({
       'pattern':     new RegExp('^' + config.prefix + 'bot\\s*([0-9]+)?$', 'i'),
-      'description': 'display my bot command list',
       'example':     config.prefix + 'bot <page number>',
+      'name':        'bot',
+      'description': 'display my bot command list',
       'callback':    'botCommandList',
       'category':    'help',
-      'cooldown':    2000,
+      'cooldown':    0,
       'disabled':    false,
       'private':     false,
       'nsfw':        false
    });
-   callbacks.botCommandList = function(message, page) {
+   data.callbacks.botCommandList = function(message, page) {
       var pagination = utilities.pagination(utilities.arraySelect(data.commands, function(command) {
-         return !command.disabled && !command.private && command.category === 'bot';
+         return command.category === 'bot' && !command.disabled && !command.private && (!command.nsfw || command.nsfw && message.channel.nsfw);
       }, function(command) {
-         return {
-            'name':   command.description,
-            'value':  '``' + command.example + '``',
-            'inline': false
-         };
+         return {'name': command.description, 'value': '``' + command.example + '``', 'inline': false};
       }), config.pagination, page);
       message.channel.send({'embed': {
          'color':  config.embedColor,
