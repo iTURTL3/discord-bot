a6857948003aaff25698890a613688613aeb79d6
jakemadness <j4kem4dness@live.com>
2019-09-22 15:36:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
-var bot       = new (require('discord.js')).Client();
-var config    = new (require('./includes/config.js'))();
-var data      = new (require('./includes/data.js'))();
-var utilities = new (require('./includes/utilities.js'))();
-var callbacks = new (require('./includes/callbacks.js'))();
-var events    = new (require('./includes/events.js'))(config, data, utilities, callbacks);
-require('./commands/register.js')(bot, config, data, utilities, callbacks);
+// modules.
+var config       = new (require('./includes/config.js'))();
+var data         = new (require('./includes/data.js'))();
+var utilities    = new (require('./includes/utilities.js'))();
+var dependencies = new (require('./includes/dependencies.js'))();
+var events       = new (require('./includes/events.js'))(config, data, utilities);
+var bot          = new dependencies.discordjs.Client();
+// register commands.
+require('./commands/register.js')(bot, config, data, utilities, dependencies);
+// set events.
 bot.on('error',   events.error);
 bot.on('ready',   events.ready);
 bot.on('message', events.message);
