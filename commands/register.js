f5a2655b58d63ccc6afcc5edf1e84455d3acaac2
jakemadness <j4kem4dness@live.com>
2019-09-22 16:22:24 +0100

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,19 +1,19 @@
 module.exports = function(bot, config, data, utilities, dependencies) {
    var files = [
       'misc/avatar',
-      'misc/say',
       'misc/cage',
+      'misc/say',
+      'bot/about',
+      'bot/info',
       'bot/invite',
       'bot/ping',
       'bot/uptime',
-      'bot/servers',
-      'bot/version',
-      'mod/kickuser',
-      'mod/banuser',
-      'help/help',
       'help/bot',
+      'help/help',
       'help/misc',
-      'help/mod'
+      'help/mod',
+      'mod/kickuser',
+      'mod/banuser'
    ];
    for ( var i = 0, length = files.length; i < length; i++ ) {
       require('./' + files[i] + '.js')(bot, config, data, utilities, dependencies);
