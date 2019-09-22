7f35ce804534e1f1c363c72f6bdd4f7a64d7e316
jakemadness <j4kem4dness@live.com>
2019-09-22 16:15:31 +0100

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,24 +1,21 @@
-module.exports = function(bot, config, data, utilities, callbacks) {
+module.exports = function(bot, config, data, utilities, dependencies) {
    data.commands.push({
       'pattern':     new RegExp('^' + config.prefix + 'help\\s*([0-9]+)?$', 'i'),
-      'description': 'display my command list',
       'example':     config.prefix + 'help <page number>',
+      'name':        'help',
+      'description': 'display my command list',
       'callback':    'helpCommandList',
       'category':    'help',
-      'cooldown':    2000,
+      'cooldown':    0,
       'disabled':    false,
       'private':     false,
       'nsfw':        false
    });
-   callbacks.helpCommandList = function(message, page) {
+   data.callbacks.helpCommandList = function(message, page) {
       var pagination = utilities.pagination(utilities.arraySelect(data.commands, function(command) {
-         return !command.disabled && !command.private && command.category === 'help';
+         return command.category === 'help' && !command.disabled && !command.private && (!command.nsfw || command.nsfw && message.channel.nsfw);
       }, function(command) {
-         return {
-            'name':   command.description,
-            'value':  '``' + command.example + '``',
-            'inline': false
-         };
+         return {'name': command.description, 'value': '``' + command.example + '``', 'inline': false};
       }), config.pagination, page);
       message.channel.send({'embed': {
          'color':  config.embedColor,
