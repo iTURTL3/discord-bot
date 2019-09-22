47fefa0ffab30cdadd529c2320b3b2ed4c952a21
jakemadness <j4kem4dness@live.com>
2019-09-22 15:54:54 +0100

    Create about.js

---
+++ commands/bot/about.js
@@ -0,0 +1,53 @@
+module.exports = function(bot, config, data, utilities, dependencies) {
+   data.commands.push({
+      'pattern':     new RegExp('^' + config.prefix + 'about$', 'i'),
+      'example':     config.prefix + 'about',
+      'name':        'about',
+      'description': 'check information about me',
+      'callback':    'aboutMe',
+      'category':    'bot',
+      'cooldown':    0,
+      'disabled':    false,
+      'private':     false,
+      'nsfw':        false
+   });
+   data.callbacks.aboutMe = function(message, name) {
+      var time = utilities.msToDuration(bot.uptime);
+      message.channel.send({'embed': {
+         'color':  config.embedColor,
+         'title':  config.name + ' - About Me',
+         'fields': [
+            {
+               'name':   'ping',
+               'value':  utilities.shortenFloat(bot.ping, 2) + 'ms!',
+               'inline': true
+            },
+            {
+               'name':   'uptime',
+               'value':  time['0'] + 'd ' + time['1'] + 'h ' + time['2'] + 'm ' + time['3'] + 's!',
+               'inline': true
+            },
+            {
+               'name':   'servers',
+               'value':  bot.guilds.size,
+               'inline': true
+            },
+            {
+               'name':   'version',
+               'value':  config.version,
+               'inline': true
+            },
+            {
+               'name':   'commands',
+               'value':  data.commands.length,
+               'inline': true
+            },
+            {
+               'name':   'invite',
+               'value':  '[invite link!](' + config.invite + ')',
+               'inline': true
+            }
+         ]
+      }});
+   };
+};
