de891dba0da5fca3d542b44f48a47d8abb65db99
jakemadness <j4kem4dness@live.com>
2019-09-22 15:52:10 +0100

    Update ping.js

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -1,16 +1,17 @@
-module.exports = function(bot, config, data, utilities, callbacks) {
+module.exports = function(bot, config, data, utilities, dependencies) {
    data.commands.push({
       'pattern':     new RegExp('^' + config.prefix + 'ping$', 'i'),
+      'example':     'ping',
+      'name':        'ping',
       'description': 'check my ping latency',
-      'example':     config.prefix + 'ping',
-      'callback':    'pingPong',
+      'callback':    'botLatency',
       'category':    'bot',
-      'cooldown':    2000,
+      'cooldown':    0,
       'disabled':    false,
       'private':     false,
       'nsfw':        false
    });
-   callbacks.pingPong = function(message) {
+   data.callbacks.botLatency = function(message) {
       message.channel.send({'embed': {
          'color':       config.embedColor,
          'title':       config.name + ' - Ping',
