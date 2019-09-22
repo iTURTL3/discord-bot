160f9b33b3f0806af1e222cd21b0c34075262d23
jakemadness <j4kem4dness@live.com>
2019-09-22 15:39:43 +0100

    Update events.js

--- includes/events.js
+++ includes/events.js
@@ -1,10 +1,10 @@
-module.exports = function(config, data, utilities, callbacks) {
+module.exports = function(config, data, utilities) {
    var self   = this;
    self.error = function(error) {
       console.log(error);
    };
    self.ready = function() {
-      this.user.setActivity('You, ' + config.prefix + 'help', {type: 'WATCHING'});
+      this.user.setActivity('You, ' + config.prefix + 'help', {'type': 'WATCHING'});
    };
    self.message = function(message) {
       if ( message.content.indexOf(config.prefix) === 0 && !message.author.bot ) {
@@ -12,19 +12,25 @@ module.exports = function(config, data, utilities, callbacks) {
             match = message.content.match(data.commands[i].pattern);
             if ( match ) {
                if ( data.commands[i].disabled ) {
-                  message.channel.send('this command is currently disabled!');
+                  message.channel.send('The **' + data.commands[i].name + '** command is currently disabled!');
                }
                else if ( data.commands[i].nsfw && !message.channel.nsfw ) {
-                  message.channel.send('you can only use this command in nsfw channels!');
+                  message.channel.send('You can only use the **' + data.commands[i].name + '** command in NSFW channels!');
                }
                else if ( message.author.id in data.cooldowns ) {
-                  message.channel.send('you can use another command in **' + utilities.shortenFloat(Math.max(0.01, (data.cooldowns[message.author.id] - message.createdTimestamp) / 1000), 2) + '**s!');
+                  message.channel.send('You can use another command in **' + utilities.shortenFloat(Math.max(0.01, (data.cooldowns[message.author.id] - message.createdTimestamp) / 1000), 2) + '**s!');
+               }
+               else if ( message.author.id + data.commands[i].name in data.cooldowns ) {
+                  message.channel.send('You can use the **' + data.commands[i].name + '** command again in **' + utilities.shortenFloat(Math.max(0.01, (data.cooldowns[message.author.id + data.commands[i].name] - message.createdTimestamp) / 1000), 2) + '**s!');
                }
                else {
                   match.shift();
                   match.unshift(message);
-                  callbacks[data.commands[i].callback].apply(null, match);
-                  utilities.objectSetAndDelete(data.cooldowns, message.author.id, message.createdTimestamp + data.commands[i].cooldown, data.commands[i].cooldown);
+                  data.callbacks[data.commands[i].callback].apply(null, match);
+                  utilities.objectSetAndDelete(data.cooldowns, message.author.id, message.createdTimestamp + config.cooldown, config.cooldown);
+                  if ( data.commands[i].cooldown > config.cooldown ) {
+                     utilities.objectSetAndDelete(data.cooldowns, message.author.id + data.commands[i].name, message.createdTimestamp + data.commands[i].cooldown, data.commands[i].cooldown);
+                  }
                }
                break;
             }
